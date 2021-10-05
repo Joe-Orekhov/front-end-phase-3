69a5b50804b9cce6a6d26d5c29f738384710411b
joesephorekhov <josephorekhov@gmail.com>
2021-10-05 16:27:46 -0600

    Basic Routing

--- front-end/src/component/login.js
+++ front-end/src/component/login.js
@@ -1,12 +1,23 @@
 import Student from "./student"
 import Tutor from "./tutor"
+import { Switch, Link, Route, BrowserRouter } from 'react-router-dom'
 
 function Login() {
   return(
     <div>
       <h1>LOGIN</h1>
-      <Student />
-      <Tutor />
+      <BrowserRouter>
+        <button><Link to ='/student'>Student</Link></button>
+        <button><Link to ='/tutor'>Tutor</Link></button>
+        <Switch>
+          <Route path='/student'>
+            <Student />
+          </Route >
+          <Route path='/tutor'>
+            <Tutor />
+          </Route>
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
--- front-end/src/component/navBar.js
+++ front-end/src/component/navBar.js
@@ -1,14 +1,28 @@
 import Profile from "./profile"
 import Requests from "./requests"
 import Schedule from "./schedule"
+import { Switch, Link, Route, BrowserRouter } from 'react-router-dom'
 
 function Navebar() {
   return(
     <div>
       <h2>Navebar</h2>
-      <Profile />
-      <Requests />
-      <Schedule />
+      <BrowserRouter>
+        <button><Link to='/profile'>Profile</Link></button>
+        <button><Link to='/requests'>Requests</Link></button>
+        <button><Link to='/schedule'>Schedule</Link></button>
+        <Switch>
+          <Route path='/profile'>
+            <Profile />
+          </Route >
+          <Route path='/requests'>
+            <Requests />
+          </Route >
+          <Route path='/schedule'>
+            <Schedule />
+          </Route >
+        </Switch>
+      </BrowserRouter>
     </div>
   )
 }
