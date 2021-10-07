baa576b1b0f89295f8b232adaf3df5d81bdeeb65
joesephorekhov <josephorekhov@gmail.com>
2021-10-07 17:52:42 -0600

    PATCH Edit Form

--- front-end/src/component/StudentProfile.js
+++ front-end/src/component/StudentProfile.js
@@ -1,17 +1,23 @@
 import { useEffect, useState } from "react"
+import EditForm from './StudentEditForm'
 
 function StudentProfile({userId}) {
   const [studentInfo, setStudentInfo] = useState([])
+  const [editForm, setEditForm] = useState(true)
   
   useEffect(() => {
     fetch(`http://localhost:9292/students/${userId}/profile`)
     .then(res => res.json())
     .then(setStudentInfo)
-  }, userId)
+  }, [userId])
+
+
 
   return(
     <div>
+      
       <h1>My Profile</h1>
+      {editForm? <button onClick={()=> setEditForm(false)}>Edit Profile</button> : <div><button onClick={() =>setEditForm(true)}>Exit Edit</button> <EditForm studentInfo={studentInfo} userId={userId} setStudentInfo={setStudentInfo}/> </div> }
       <h2>Name: {studentInfo.name}</h2>
       <img src={studentInfo.img} alt="studentpic" width="400" height="400" />
       <p>Grade: {studentInfo.grade}</p>
