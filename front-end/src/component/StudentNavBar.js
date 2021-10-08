ce1d97f11738d82a60a490f9ce0ab546b3a26d21
joesephorekhov <josephorekhov@gmail.com>
2021-10-08 13:49:14 -0600

    Final css from joe

--- front-end/src/component/StudentNavBar.js
+++ front-end/src/component/StudentNavBar.js
@@ -6,6 +6,10 @@ function StudentNavBar() {
   return(
 
     <div className="navbar">
+      <div id='logoDiv'>
+        <img id='acornLogo' src='https://i.pinimg.com/originals/5a/a4/53/5aa453cd95c44fc945047d870af2de66.png' alt='LOGO'></img>
+        <p id='acornPhrase'>Where A Seed<br/> Becomes a Tree</p>
+      </div>
       <Link to="/"><button className="nav-tab">Login</button></Link>
       <Link to="/sign_up"><button className="nav-tab">Sign Up</button></Link>
       <Link to="/student/schedule"><button className="nav-tab">My Schedule</button></Link>
